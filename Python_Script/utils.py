96d184a6d12e8df48e68afccc6d4407693a1d90a
Ayo <ayooluwaadedipe@gmail.com>
2023-01-24 20:02:15 +0000

    Extend pawn threatened sqaures to optionally cover valid moves

--- Python_Script/utils.py
+++ Python_Script/utils.py
@@ -22,7 +22,7 @@ def normalized_arithmetic(color, operation, value1, value2):
     elif(operation == "diff"):
       return value1 - value2
 
-def pawn_threatened_squares(current_position, current_board):
+def pawn_threatened_squares(current_position, current_board, validity_check=False):
     pawn_threats = []
     x_coord = squares[current_position][0]
     y_coord = squares[current_position][1]
@@ -49,6 +49,22 @@ def pawn_threatened_squares(current_position, current_board):
         pawn_threats.append(left_target_position)
     if(top_left_square_is_valid and len(left_target_piece)>=1 and left_target_piece[0]!=color):
         pawn_threats.append(left_target_position)
+    if(validity_check):
+      base_row_dict = {"w": 1, "b": 6}
+      PAWN_HAS_NOT_MOVED = y_coord == base_row_dict[str(color).lower()]
+      first_square_ahead_y_coord = normalized_arithmetic(color, "sum", y_coord, 1)
+      first_square_ahead_is_valid = isValidBoardCoordinates(x_coord, first_square_ahead_y_coord)
+      first_square_ahead_position = inverted_squares_map[str(x_coord)+','+str(first_square_ahead_y_coord)] if first_square_ahead_is_valid else 'invalid_square'
+      first_square_ahead_piece = current_board[first_square_ahead_position][2] if first_square_ahead_is_valid else 'invalid_piece'
+      second_square_ahead_y_coord = normalized_arithmetic(color, "sum", y_coord, 2)
+      second_square_ahead_is_valid = isValidBoardCoordinates(x_coord, second_square_ahead_y_coord)
+      second_square_ahead_position = inverted_squares_map[str(x_coord)+','+str(second_square_ahead_y_coord)] if second_square_ahead_is_valid else 'invalid_square'
+      second_square_ahead_piece = current_board[second_square_ahead_position][2] if second_square_ahead_is_valid else 'invalid_piece'
+      if(first_square_ahead_is_valid and len(first_square_ahead_piece)==0):
+          pawn_threats.append(first_square_ahead_position)
+      if(second_square_ahead_is_valid and first_square_ahead_is_valid and PAWN_HAS_NOT_MOVED and len(second_square_ahead_piece) == 0 and len(first_square_ahead_piece)==0):
+          pawn_threats.append(second_square_ahead_position)
+
     return pawn_threats
 
 def knight_threatened_squares(current_position, current_board):
@@ -464,28 +480,30 @@ def all_threatened_squares(current_board):
 
 ### Tests
 
-sampleBoard = {"a1":[0,0,""],"b1":[1,0,"WQN"],"c1":[2,0,""],"d1":[3,0,""],"e1":[4,0,""], "f1":[5,0,"WKB"], "g1":[6,0,""], "h1":[7,0,"WKR"], 
+# sampleBoard = {"a1":[0,0,""],"b1":[1,0,"WQN"],"c1":[2,0,""],"d1":[3,0,""],"e1":[4,0,""], "f1":[5,0,"WKB"], "g1":[6,0,""], "h1":[7,0,"WKR"], 
          
-"a2":[0,1,"Wp1"],"b2":[1,1,"Wp2"],"c2":[2,1,"Wp3"],"d2":[3,1,"Wp4"],"e2":[4,1,"Wp5"], "f2":[5,1,"Wp6"], "g2":[6,1,"Wp7"], "h2":[7,1,"Wp8"],
+# "a2":[0,1,""],"b2":[1,1,"Wp2"],"c2":[2,1,"Wp3"],"d2":[3,1,"Wp4"],"e2":[4,1,"Wp5"], "f2":[5,1,"Wp6"], "g2":[6,1,"Wp7"], "h2":[7,1,"Wp8"],
          
-"a3":[0,2,""],"b3":[1,2,""],"c3":[2,2,""],"d3":[3,2,""],"e3":[4,2,""], "f3":[5,2,"WKN"], "g3":[6,2,""], "h3":[7,2,""],
+# "a3":[0,2,"Wp1"],"b3":[1,2,""],"c3":[2,2,""],"d3":[3,2,""],"e3":[4,2,""], "f3":[5,2,"WKN"], "g3":[6,2,""], "h3":[7,2,""],
 
-"a4":[0,3,""],"b4":[1,3,""],"c4":[2,3,"WQR"],"d4":[3,3,""],"e4":[4,3,"WK"], "f4":[5,3,""], "g4":[6,3,"WQ"], "h4":[7,3,""],  
+# "a4":[0,3,""],"b4":[1,3,""],"c4":[2,3,"WQR"],"d4":[3,3,""],"e4":[4,3,"WK"], "f4":[5,3,""], "g4":[6,3,"WQ"], "h4":[7,3,""],  
 
-"a5":[0,4,""],"b5":[1,4,"BQ"],"c5":[2,4,""],"d5":[3,4,"BQB"],"e5":[4,4,""], "f5":[5,4,""], "g5":[6,4,"BKR"], "h5":[7,4,""],
+# "a5":[0,4,""],"b5":[1,4,"BQ"],"c5":[2,4,""],"d5":[3,4,"BQB"],"e5":[4,4,""], "f5":[5,4,""], "g5":[6,4,"BKR"], "h5":[7,4,""],
 
-"a6":[0,5,""],"b6":[1,5,""],"c6":[2,5,"BK"],"d6":[3,5,""],"e6":[4,5,"WQB"], "f6":[5,5,"BKN"], "g6":[6,5,""], "h6":[7,5,""],
+# "a6":[0,5,""],"b6":[1,5,""],"c6":[2,5,"BK"],"d6":[3,5,""],"e6":[4,5,"WQB"], "f6":[5,5,"BKN"], "g6":[6,5,""], "h6":[7,5,""],
 
-"a7":[0,6,"Bp8"],"b7":[1,6,"Bp7"],"c7":[2,6,"Bp6"],"d7":[3,6,"Bp5"],"e7":[4,6,"Bp4"], "f7":[5,6,"Bp3"], "g7":[6,6,"Bp2"], "h7":[7,6,"Bp1"], 
+# "a7":[0,6,"Bp8"],"b7":[1,6,"Bp7"],"c7":[2,6,"Bp6"],"d7":[3,6,"Bp5"],"e7":[4,6,"Bp4"], "f7":[5,6,"Bp3"], "g7":[6,6,"Bp2"], "h7":[7,6,"Bp1"], 
 
-"a8":[0,7,"BQR"],"b8":[1,7,"BQN"],"c8":[2,7,""],"d8":[3,7,""],"e8":[4,7,""], "f8":[5,7,"BKB"], "g8":[6,7,""], "h8":[7,7,""]
+# "a8":[0,7,"BQR"],"b8":[1,7,"BQN"],"c8":[2,7,""],"d8":[3,7,""],"e8":[4,7,""], "f8":[5,7,"BKB"], "g8":[6,7,""], "h8":[7,7,""]
          
-         }
+#          }
 
-# print(pawn_threatened_squares('e7', sampleBoardWhite)) #['d6', 'f6']
-# print(pawn_threatened_squares('c2', sampleBoardWhite)) #['d3', 'b3']
-# print(knight_threatened_squares('f3', sampleBoardWhite)) #['h4', 'g5', 'd4', 'e5', 'g1', 'e1']
-# print(knight_threatened_squares('f6', sampleBoardWhite)) #['d5', 'e4', 'h5', 'g4', 'g8']
+# print(pawn_threatened_squares('e7', sampleBoard)) #['d6', 'f6']
+# print(pawn_threatened_squares('d2', sampleBoard, True)) #['e3', 'c3', 'd3', 'd4']
+# print(pawn_threatened_squares('a3', sampleBoard, True)) #['b4', 'a4']
+# print(pawn_threatened_squares('c2', sampleBoard)) #['d3', 'b3']
+# print(knight_threatened_squares('f3', sampleBoard)) #['h4', 'g5', 'd4', 'e5', 'g1', 'e1']
+# print(knight_threatened_squares('f6', sampleBoard)) #['d5', 'e4', 'h5', 'g4', 'g8']
 # print(rook_threatened_squares('c4', sampleBoard)) #['d4', 'b4', 'a4', 'c5', 'c6', 'c7', 'c3']
 # print(rook_threatened_squares('g5', sampleBoard)) #['f5', 'e5', 'd5', 'c5', 'b5', 'a5', 'h5', 'g4', 'g6']
 # print(bishop_threatened_squares('e6', sampleBoard)) #['f7', 'f5', 'd7', 'd5']
