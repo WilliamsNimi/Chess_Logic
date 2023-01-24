eb864c62eb66bb0685fae994bc8169b621fe6e64
Ayo <ayooluwaadedipe@gmail.com>
2023-01-24 13:51:08 +0000

    queen function test

--- Python_Script/utils.py
+++ Python_Script/utils.py
@@ -322,8 +322,6 @@ def queen_threatened_squares(current_position, current_board):
     A Queen combines the moves of both the Bishop and Rook.
     We'll simply call the existing functions and combine their results
     """
-
-    color = current_board[current_position][2][0]
     bishop_threats = bishop_threatened_squares(current_position, current_board)
     rook_threats = rook_threatened_squares(current_position, current_board)
     return bishop_threats + rook_threats
@@ -461,13 +459,13 @@ sampleBoard = {"a1":[0,0,""],"b1":[1,0,"WQN"],"c1":[2,0,""],"d1":[3,0,""],"e1":[
 
 "a4":[0,3,""],"b4":[1,3,""],"c4":[2,3,"WQR"],"d4":[3,3,""],"e4":[4,3,"WK"], "f4":[5,3,""], "g4":[6,3,"WQ"], "h4":[7,3,""],  
 
-"a5":[0,4,""],"b5":[1,4,""],"c5":[2,4,""],"d5":[3,4,"BQB"],"e5":[4,4,""], "f5":[5,4,""], "g5":[6,4,"BKR"], "h5":[7,4,""],
+"a5":[0,4,""],"b5":[1,4,"BQ"],"c5":[2,4,""],"d5":[3,4,"BQB"],"e5":[4,4,""], "f5":[5,4,""], "g5":[6,4,"BKR"], "h5":[7,4,""],
 
 "a6":[0,5,""],"b6":[1,5,""],"c6":[2,5,"BK"],"d6":[3,5,""],"e6":[4,5,"WQB"], "f6":[5,5,"BKN"], "g6":[6,5,""], "h6":[7,5,""],
 
 "a7":[0,6,"Bp8"],"b7":[1,6,"Bp7"],"c7":[2,6,"Bp6"],"d7":[3,6,"Bp5"],"e7":[4,6,"Bp4"], "f7":[5,6,"Bp3"], "g7":[6,6,"Bp2"], "h7":[7,6,"Bp1"], 
 
-"a8":[0,7,"BQR"],"b8":[1,7,"BQN"],"c8":[2,7,""],"d8":[3,7,"BQ"],"e8":[4,7,""], "f8":[5,7,"BKB"], "g8":[6,7,""], "h8":[7,7,""]
+"a8":[0,7,"BQR"],"b8":[1,7,"BQN"],"c8":[2,7,""],"d8":[3,7,""],"e8":[4,7,""], "f8":[5,7,"BKB"], "g8":[6,7,""], "h8":[7,7,""]
          
          }
 
@@ -479,8 +477,10 @@ sampleBoard = {"a1":[0,0,""],"b1":[1,0,"WQN"],"c1":[2,0,""],"d1":[3,0,""],"e1":[
 # print(rook_threatened_squares('g5', sampleBoard)) #['f5', 'e5', 'd5', 'c5', 'b5', 'a5', 'h5', 'g4', 'g6']
 # print(bishop_threatened_squares('e6', sampleBoard)) #['f7', 'f5', 'd7', 'd5']
 # print(bishop_threatened_squares('d5', sampleBoard)) #['c4', 'c6', 'e4', 'e6']
-print(king_threatened_squares('e4', sampleBoard)) #['e5', 'f5', 'e5', 'f3', 'e3', 'd3', 'e3', 'd5']
-print(king_threatened_squares('c6', sampleBoard)) #['e5', 'f5', 'e5', 'f3', 'e3', 'd3', 'e3', 'd5']
+print(queen_threatened_squares('g4', sampleBoard)) #['h5', 'h3', 'f5', 'h4', 'f4', 'g5', 'g3']
+print(queen_threatened_squares('b5', sampleBoard)) #['a4', 'a6', 'c4', 'a5', 'c5', 'b4', 'b3', 'b2', 'b6']
+# print(king_threatened_squares('e4', sampleBoard)) #['e5', 'f5', 'e5', 'f3', 'e3', 'd3', 'e3', 'd5']
+# print(king_threatened_squares('c6', sampleBoard)) #['e5', 'f5', 'e5', 'f3', 'e3', 'd3', 'e3', 'd5']
 # print(extract_piece_name_and_color("Bk"))
 
 #print(all_threatened_squares(sampleBoardWhite))
